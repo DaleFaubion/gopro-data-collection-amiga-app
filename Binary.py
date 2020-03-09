58b04e27ca686118e27d8abeea27fbecf034e713
GCyrus <buschwookie@gmail.com>
2020-03-09 20:36:46 +0000

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -1,4 +1,3 @@
-
 import re
 from argparse import ArgumentParser
 from getpass import getpass
@@ -10,20 +9,12 @@ import csv
 import psycopg2 as pg
 
 
-
-
-
-
-
 def get_binary()
     cursor = db.cursor\
     sql = "select image_binary from image where image_id = 1"
     cursor.execute(sql)
     image_binary = cursor.fetchone()[0]
     return image_binary
-    
-
-
 
 if __name__ == "__main__";
 
