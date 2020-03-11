4a22a2552c502e96b5cfb79b42bcd201dedd4b64
GCyrus <buschwookie@gmail.com>
2020-03-11 20:08:11 +0000

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -9,14 +9,20 @@ import csv
 import psycopg2 as pg
 
 
-def get_binary()
-    cursor = db.cursor\
+def get_binary():
+    
+    dbname = "vinetech"
+    username = "cmeade16"
+    password = "fQcm3aPh"
+    
+    db = pg.connect("host='localhost' dbname='%s' user='%s' password='%s'" % (db_name, username, password))
+    cursor = db.cursor
     sql = "select image_binary from image where image_id = 1"
     cursor.execute(sql)
     image_binary = cursor.fetchone()[0]
     return image_binary
 
-if __name__ == "__main__";
+if __name__ == "__main__":
 
     image_binary = get_binary()
     f = open("verifyFile.jpg")
