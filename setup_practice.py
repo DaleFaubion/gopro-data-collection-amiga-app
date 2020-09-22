0c2f67390feff6815378d351901342b2d33f7789
Jack Reamy <jreamy17@goergefox.edu>
2020-09-21 23:47:44 -0700

    setting up Fall 2020 onboarding

---
+++ setup_practice.py
@@ -0,0 +1,17 @@
+#
+
+# Fall 2020
+# Vinetech ingest practice setup
+
+date = "2019-07-31"
+vineyard = "crawford-beck"
+block = 9
+
+image_path = os.path.join("~/ingest", "raw_images")
+image_out_path = f_org.get_image_path(vineyard, block, date)
+
+label_path = f_org.get_label_path(vineyard, block, date)
+label_file = os.path.join(label_path, "locations.csv")
+
+print(image_path)
+print(image_out_path)
