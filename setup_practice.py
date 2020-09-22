5d93b713268adcaf512c3ad7dac0e592f46004cf
Jack Reamy <jreamy17@goergefox.edu>
2020-09-22 00:37:25 -0700

    symlink setup

--- setup_practice.py
+++ setup_practice.py
@@ -13,13 +13,27 @@ date = "2019-07-31"
 vineyard = "crawford-beck"
 block = 9
 
-image_path = os.path.join("~/ingest", "raw_images")
+image_path = f_org.ensure_path("~/ingest", "raw_images")
 image_out_path = f_org.get_image_path(vineyard, block, date)
 
 label_path = f_org.get_label_path(vineyard, block, date)
 label_file = os.path.join(label_path, "locations.csv")
 labels = pd.read_csv(label_file)
 
-print(image_path)
-print(image_out_path)
-print(labels[["name", "angle"]].head())
+x = 0
+
+
+def make_symlinks(anlge, dirname):
+
+    for image in labels.loc[labels["angle"] == angle, "name"]:
+        os.symlink(
+            os.path.join(image_out_path, image),
+            os.path.join(image_path, dirname, str(x) + ".jpg"),
+        )
+
+        x += 1
+
+
+make_symlinks(0, "top_camera")
+make_symlinks(1, "bottom_camera")
+make_symlinks(2, "middle_camera")
