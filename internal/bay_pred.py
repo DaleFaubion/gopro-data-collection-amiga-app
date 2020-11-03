825b16e7f10756aceea75eba0adbb8be6ffad791
Jack Reamy <jreamy17@goergefox.edu>
2020-11-02 19:37:39 -0800

    bay model info

--- internal/bay_pred.py
+++ internal/bay_pred.py
@@ -75,6 +75,10 @@ def main(f_org, args, df=None):
     # Train on 80% of the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     model = ensemble.RandomForestClassifier(n_estimators=100, max_depth=9)
+
+    print(X_train.info())
+    print(y_train.info())
+
     model.fit(X_train, y_train)
 
     # Evaluate the model on the test data
