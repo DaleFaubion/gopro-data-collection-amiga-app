7acda0470a105c2e318b742ba784b31563c15488
Jack Reamy <jreamy17@goergefox.edu>
2021-01-28 13:15:09 -0800

    try readlink

--- internal/rename_files.py
+++ internal/rename_files.py
@@ -23,7 +23,7 @@ def main(f_org, args, df=None):
         lambda x: x[x.find(args.date) + len(args.date) + 1 :]
     )
 
-    df["raw_dir"] = df["raw_dir"].apply(os.path.readlink)
+    df["raw_dir"] = df["raw_dir"].apply(os.readlink)
 
     df[columns].to_csv(label_file)
     return df[columns]
