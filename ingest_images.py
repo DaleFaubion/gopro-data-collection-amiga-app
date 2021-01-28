5f75df1372e79ac83ea3006db25e8daf7d344207
Jack Reamy <jreamy17@goergefox.edu>
2021-01-28 11:41:45 -0800

    adding more detailed failures

--- ingest_images.py
+++ ingest_images.py
@@ -34,6 +34,12 @@ def main(args):
     main ingests the images indicated by the passed args object.
     """
 
+    for linkname in ["vinetech_data", "images", "labels"]:
+        if not os.path.islink(os.path.join(f_org.home, linkname)):
+            print("symlinks haven't been set up in this environment yet")
+            print("from the root repo run source setup.sh")
+            return
+
     if not os.path.isdir(os.path.join(f_org.home, "ingest")):
         os.makedirs(os.path.join(f_org.home, "ingest"))
 
@@ -73,6 +79,7 @@ def main(args):
     # Run all the ingest steps
     df = None
     for s in steps:
+        print("Running step", s)
         df = s(f_org, args, df=df)
 
 
