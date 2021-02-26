008d3b8a09aec1f07f7b1a154bb30913200490ff
Jack Reamy <jreamy17@goergefox.edu>
2021-02-26 12:35:07 -0800

    working on image/bay linking step

--- internal/ingest_db.py
+++ internal/ingest_db.py
@@ -1,6 +1,6 @@
 # Spring 2020
 # Vinetech Database Ingest
-# Ingest Step 5
+# Ingest steps for loading the database
 
 import os
 import pandas as pd
@@ -14,7 +14,28 @@ class DB_Ingester:
     def __init__(self, db):
         self.db = db
 
+    def add_uuids(self, f_org, args, df=None):
+        # Step 0
+
+        # Open the current csv
+        label_file = f_org.get_label_file(args.vineyard, args.block, args.date)
+        if df is None:
+            df = pd.read_csv(label_file, index_col=False)
+
+        # Fill in any missing image column data
+        if not "image_id" in df.columns:
+            df["image_id"] = [str(uuid.uuid4()) for _ in df.index]
+        else:
+            missing = df[df["image_id"].isnull()].index
+            df.loc[missing, "image_id"] = [str(uuid.uuid4()) for _ in missing]
+
+        # Save and return
+        df.to_csv(label_file, index=False)
+        return df
+
     def add_bays(self, f_org, args, df=None):
+        # Step 1
+
         # Open the current csv
         label_file = f_org.get_label_file(args.vineyard, args.block, args.date)
         if df is None:
@@ -34,24 +55,9 @@ class DB_Ingester:
         df.to_csv(label_file, index=False)
         return df
 
-    def add_uuids(self, f_org, args, df=None):
-        # Open the current csv
-        label_file = f_org.get_label_file(args.vineyard, args.block, args.date)
-        if df is None:
-            df = pd.read_csv(label_file, index_col=False)
-
-        # Fill in any missing image column data
-        if not "image_id" in df.columns:
-            df["image_id"] = [str(uuid.uuid4()) for _ in df.index]
-        else:
-            missing = df[df["image_id"].isnull()].index
-            df.loc[missing, "image_id"] = [str(uuid.uuid4()) for _ in missing]
-
-        # Save and return
-        df.to_csv(label_file, index=False)
-        return df
-
     def add_metadata(self, f_org, args, df=None):
+        # Step 2
+
         # Open the current csv
         label_file = f_org.get_label_file(args.vineyard, args.block, args.date)
         if df is None:
@@ -69,19 +75,21 @@ class DB_Ingester:
         return df
 
     def add_images_to_bays(self, f_org, args, df=None):
+        # Step 3
+
         # Open the current csv
         label_file = f_org.get_label_file(args.vineyard, args.block, args.date)
         if df is None:
             df = pd.read_csv(label_file, index_col=False)
 
         # Add the predicted bays
-        valid = df[df["pred_bay"].notna()].index
-        entries = list(df.loc[valid, ["row", "pred_bay", "id"]].to_records(index=False))
+        valid = df[df["pred_bay"].notna()]
+        entries = list(valid[["row", "pred_bay", "id"]].to_records(index=False))
         self.db.add_images_to_bays(args.vineyard, args.block, "true", "pred", entries)
 
         # Add the real bays (if they exist...)
-        valid = df[df["bay"].notna()].index
-        entries = list(df.loc[valid, ["row", "pred_bay", "id"]].to_records(index=False))
+        valid = df[df["bay"].notna()]
+        entries = list(valid[["row", "pred_bay", "id"]].to_records(index=False))
         self.db.add_images_to_bays(args.vineyard, args.block, "true", "true", entries)
 
         # Save and return
@@ -89,6 +97,8 @@ class DB_Ingester:
         return df
 
     def add_image_bytes(self, f_org, args, df=None, batch_size=64):
+        # Step 4
+
         # Open the current csv
         label_file = f_org.get_label_file(args.vineyard, args.block, args.date)
         if df is None:
