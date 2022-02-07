bc9625af1030184e8d940bc8332fd60ae8111be7
Trey Moen <trey@moen.ai>
2022-02-07 09:36:16 -0800

    add dbconf argument to ingest script

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -9,7 +9,7 @@ image_dir=/srv/projects/vinetech/images/crawford-beck/block09/$date
 location_file=/tmp/locations/$date/locations.csv
 
 if [ -f $harvest_file ]; then
-  python3 ../ingest_images.py -resize 400 300 -d $date -i $image_dir $harvest_file $location_file
+  python3 ../ingest_images.py -db ../db.conf -resize 400 300 -d $date -i $image_dir $harvest_file $location_file
 else
   echo $harvest_file harvest file does not exist
   exit 1
