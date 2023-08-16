06d0bd0c62c5128a41ed369acf1b386e493ae1a0
Walker Orr <jorr@georgefox.edu>
2023-08-16 14:31:34 -0700

    removed old files

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# Fall 2020
-# Vinetech ingest imports
-
-import os.path as p
-import sys
-
-# Add the root directory to the path variables
-ingest = p.dirname(p.dirname(p.abspath(__file__)))
-sys.path.append(ingest)
