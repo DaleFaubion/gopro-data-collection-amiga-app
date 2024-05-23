9e2de6d49e10328368f103d24d569b9e3f5d1c56
Walker Orr <jorr@georgefox.edu>
2024-05-23 16:19:55 -0700

    copied over relevant code

--- post/row/rover_predrow.go
+++ post/row/rover_predrow.go
@@ -13,6 +13,87 @@ type Image struct {
 	direction string
 }
 
+type Row struct {
+	rowNum int
+	images []Image
+}
+
+// AppendImage appends a new image to the row, returns a new row
+func (row *Row) AppendImage(image Image) Row {
+	newImages := append(row.images, image)
+	return Row{row.rowNum, newImages}
+}
+
+// PrependImage prepends a new image to the row, returns a new row
+func (row *Row) PrependImage(image Image) Row {
+	singleton := []Image{image}
+	newImages := append(singleton, row.images...)
+	return Row{row.rowNum, newImages}
+}
+
+// PopFirst removes and returns the first image, returns a new row
+func (row *Row) PopFirst() (Image, Row) {
+	first := row.images[0]
+	rest := row.images[1:]
+	result := Row{row.rowNum, rest}
+	return first, result
+}
+
+// PopLast removes and returns the last image, returns a new row
+func (row *Row) PopLast() (Image, Row) {
+	last := row.images[len(row.images)-1]
+	rest := row.images[:len(row.images)-1]
+	result := Row{row.rowNum, rest}
+	return last, result
+}
+
+// GiveToStartOf removes an image from the end of this row and gives it to the start of the other row, returns
+// two new rows
+func (row *Row) GiveToStartOf(other *Row) (Row, Row) {
+	toGive, newLeft := row.PopLast()
+	newRight := other.PrependImage(toGive)
+	return newLeft, newRight
+}
+
+// TakeFromStartOf takes the first image from the other row and appends it to this one, returns two new rows
+func (row *Row) TakeFromStartOf(other *Row) (Row, Row) {
+	toGive, newRight := other.PopFirst()
+	newLeft := row.AppendImage(toGive)
+	return newLeft, newRight
+}
+
+// HasImages returns true if there are images in the row
+func (row *Row) HasImages() bool {
+	return len(row.images) > 0
+}
+
+// NumPosts returns the number of images that contain a post in the row
+func (row *Row) NumPosts() int {
+	count := 0
+
+	for _, image := range row.images {
+		if image.hasPost {
+			count++
+		}
+	}
+
+	return count
+}
+
+// NumEmpty returns the number of images that do not contain a post in the row
+func (row *Row) NumEmpty() int {
+	return row.NumImages() - row.NumPosts()
+}
+
+// NumImages returns the number of images in the row
+func (row *Row) NumImages() int {
+	return len(row.images)
+}
+
+// TODO make a method to generate new row assignments
+
+// TODO make a model struct
+
 // PoissonLogProb computes the log probability of a count under a Poisson distribution
 func PoissonLogProb(lambda float64, count int) float64 {
 	if count <= 0 {
@@ -33,7 +114,13 @@ func PoissonLogProb(lambda float64, count int) float64 {
 	}
 }
 
+// TODO make a function to load predicted posts
+
+// TODO make a function to show the row assignments
+
+// TODO make a function to do EM
+
 //main runs a program to predict row assignment based on post predictions and
 func main() {
-
+	//TODO finish
 }
--- post/row/rover_predrow_test.go
+++ post/row/rover_predrow_test.go
@@ -5,6 +5,173 @@ import (
 	"testing"
 )
 
+func SimpleImage() Image {
+	img := Image{
+		"test/pic/foo.jpg",
+		"2024-05-20",
+		"13:07:00",
+		false,
+		1,
+		1,
+		"West",
+	}
+
+	return img
+}
+
+func SecondImage() Image {
+	img := Image{
+		"test/pic/foo2.jpg",
+		"2024-05-21",
+		"13:024:00",
+		true,
+		1,
+		2,
+		"East",
+	}
+
+	return img
+}
+
+func ThirdImage() Image {
+	img := Image{
+		"test/pic/foo2.jpg",
+		"2024-05-21",
+		"13:024:00",
+		true,
+		1,
+		3,
+		"West",
+	}
+
+	return img
+}
+
+func TestRow_AppendImage_Empty(t *testing.T) {
+	row := Row{}
+	row = row.AppendImage(SimpleImage())
+
+	if len(row.images) != 1 || row.images[0] != SimpleImage() {
+		t.Error("Image was not added to an empty row")
+	}
+}
+
+func TestRow_AppendImage(t *testing.T) {
+	row := Row{}
+	row.images = append(row.images, SimpleImage())
+
+	row = row.AppendImage(SecondImage())
+
+	if len(row.images) != 2 || row.images[0] != SimpleImage() || row.images[1] != SecondImage() {
+		t.Error("Image was not added to a row with images")
+	}
+}
+
+func TestRow_GiveToStartOf_Empties(t *testing.T) {
+	left := Row{}
+	right := Row{}
+
+	left = left.AppendImage(SimpleImage())
+	left, right = left.GiveToStartOf(&right)
+
+	if len(left.images) != 0 {
+		t.Error("Giving row did not remove image")
+	}
+
+	if len(right.images) != 1 || right.images[0] != SimpleImage() {
+		t.Error("Receiving row did not add image")
+	}
+}
+
+func TestRow_GiveToStartOf(t *testing.T) {
+	left := Row{}
+	right := Row{}
+
+	left = left.AppendImage(SimpleImage())
+	left = left.AppendImage(ThirdImage())
+	right = right.AppendImage(SecondImage())
+
+	left, right = left.GiveToStartOf(&right)
+
+	if len(left.images) != 1 || left.images[0] != SimpleImage() {
+		t.Error("Giving row does not have the correct images after giveToStartOf")
+	}
+
+	if len(right.images) != 2 || right.images[0] != ThirdImage() || right.images[1] != SecondImage() {
+		t.Error("Receiving row did not add image")
+	}
+}
+
+func TestRow_HasImages_NonEmpty(t *testing.T) {
+	row := Row{}
+	row = row.AppendImage(SimpleImage())
+
+	if !row.HasImages() {
+		t.Error("Row did not have images")
+	}
+}
+
+func TestRow_HasImages_Empty(t *testing.T) {
+	row := Row{}
+
+	if row.HasImages() {
+		t.Error("Empty row reported to have images")
+	}
+}
+
+func TestRow_TakeFromStartOf(t *testing.T) {
+
+	left := Row{}
+	right := Row{}
+
+	left = left.AppendImage(SimpleImage())
+	right = right.AppendImage(ThirdImage())
+	right = right.AppendImage(SecondImage())
+
+	left, right = left.TakeFromStartOf(&right)
+
+	if len(left.images) != 2 || left.images[1] != ThirdImage() {
+		t.Error("The receiving row did not add the image")
+	}
+
+	if len(right.images) != 1 || right.images[0] != SecondImage() {
+		t.Error("The giving row did not have the correct number of images")
+	}
+}
+
+func TestRow_NumEmpty(t *testing.T) {
+	row := Row{}
+	row = row.AppendImage(SimpleImage())
+	row = row.AppendImage(SecondImage())
+	row = row.AppendImage(ThirdImage())
+
+	if row.NumEmpty() != 1 {
+		t.Error("Row did not have the correct number of non-post images")
+	}
+}
+
+func TestRow_NumImages(t *testing.T) {
+	row := Row{}
+	row = row.AppendImage(SimpleImage())
+	row = row.AppendImage(SecondImage())
+	row = row.AppendImage(ThirdImage())
+
+	if row.NumImages() != 3 {
+		t.Error("Row did not have the correct number of images")
+	}
+}
+
+func TestRow_NumPosts(t *testing.T) {
+	row := Row{}
+	row = row.AppendImage(SimpleImage())
+	row = row.AppendImage(SecondImage())
+	row = row.AppendImage(ThirdImage())
+
+	if row.NumPosts() != 2 {
+		t.Error("Row did not have the correct number of images with posts")
+	}
+}
+
 func Test_LogProb_1(t *testing.T) {
 	prob := PoissonLogProb(1, 1)
 
